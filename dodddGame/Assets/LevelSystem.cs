517594632ac7208de4249ad27815501ef6e7149e
1107719008 <48972788+1107719008@users.noreply.github.com>
2021-08-20 02:18:06 +0800

    doodle set up

---
+++ dodddGame/Assets/LevelSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    public GameObject platformPrefab;
+
+    public int numberOfPlat = 200;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+
+
+    void Start()
+    {
+        Vector3 spawnPos=new Vector3();
+
+        for(int i = 0; i < numberOfPlat; i++)
+        {
+            spawnPos.y += Random.Range(minY, maxY);
+            spawnPos.x = Random.Range(levelWidth, -levelWidth);
+            Instantiate(platformPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ dodddGame/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    float movement = 0f;
+    public float movementSpeed = 10f;
+
+    [SerializeField] private float EdgeR = 2;
+    [SerializeField] private float EdgeL = -2;
+
+    Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal")*movementSpeed;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    
+    }
+}
