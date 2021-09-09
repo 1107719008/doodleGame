45c32bc5552eadfa00e3a9853bba9def50f385ce
1107719008 <48972788+1107719008@users.noreply.github.com>
2021-09-10 03:12:16 +0800

    side movement ok

---
+++ dodddGame/Assets/Script/MoveSide.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSide : MonoBehaviour
+{
+
+    //public Collider2D leftT;
+    //public Collider2D rightT;
+    [SerializeField] public GameObject playerPos;
+    private bool playerTouchEdge;
+    Rigidbody2D playerRb;
+    
+
+    void Start()
+    {
+        playerTouchEdge = false;
+
+        //playerRb = playerPos.GetComponent<Rigidbody2D>();
+
+    }
+
+    
+    void Update()
+    {
+        if (playerTouchEdge == true)
+        {
+            playerPos.transform.position = new Vector2(-playerPos.transform.position.x, playerPos.transform.position.y);
+
+            playerTouchEdge = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerTouchEdge = true;
+           
+        }
+    }
+}
---
+++ dodddGame/Assets/Script/Score.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    int score ;
+    //show score ui
+    public Text showScore;
+
+    [SerializeField]
+    Vector3 _lastPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        _lastPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((this.transform.position.y - _lastPosition.y) > 0)
+            score++;
+
+        _lastPosition = this.transform.position;
+
+        showScore.text = score.ToString();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Score : " + score);
+    }
+}
